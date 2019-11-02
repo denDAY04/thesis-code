57c11e2a9eaf2c1215fab341f40bdeddfe2b1b00
Andreas Stensig <stensig04@gmail.com>
2019-11-02 22:26:04 +0100

    Project init

---
+++ src/main/java/dk/asj/dpm/UserInterface.java
@@ -0,0 +1,8 @@
+package dk.asj.dpm;
+
+public class UserInterface {
+
+    public static void main(String[] args) {
+        System.out.println("Starting Distributed Password Manager...");
+    }
+}
