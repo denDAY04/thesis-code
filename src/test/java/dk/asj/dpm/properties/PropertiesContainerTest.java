d80d42b6f4767bf75e4ce0fb2e75c05d6bc3af02
Andreas Stensig <stensig04@gmail.com>
2019-11-05 16:13:34 +0100

    Properties container test

---
+++ src/test/java/dk/asj/dpm/properties/PropertiesContainerTest.java
@@ -0,0 +1,23 @@
+package dk.asj.dpm.properties;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertiesContainerTest {
+
+    @Test
+    @DisplayName("Load properties from file")
+    void loadProperties() throws IOException {
+        PropertiesContainer properties = PropertiesContainer.loadProperties();
+
+        assertNotNull(properties, "Properties container is null");
+
+        assertNotNull(properties.getStorageProperties(),"Storage properties is null");
+        assertNotNull(properties.getStorageProperties().getFragmentPath(), "Fragment storage path is null");
+        assertNotNull(properties.getStorageProperties().getNetworkPropertiesPath(), "Network properties storage path is null");
+    }
+}
